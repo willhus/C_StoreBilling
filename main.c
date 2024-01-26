94dda1ae9bdb78210bcf76fdb8ad110f9637902c
willhus <117056232+willhus@users.noreply.github.com>
2024-01-25 19:13:28 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+//Generating Bills
+void bill_header(char name[50], char date[30])
+{
+    printf("\n\n");
+        printf("\t      William's Food Store");
+        printf("\n\t    --------------------");
+        printf("Date: %s", date);
+        printf("\nInvoice to: %s\n", name);
+        printf("--------------------");
+        printf("Items\t\t");
+        printf()
+
+}
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
